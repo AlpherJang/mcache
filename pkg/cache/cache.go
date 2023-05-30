c12b111f39316cfa4dbc49a658f116d89f85c15a
AlpherJang <85141291@qq.com>
2023-05-30 11:00:26 +0800

    support clean table list
    Signed-off-by: AlpherJang <85141291@qq.com>

--- pkg/cache/cache.go
+++ pkg/cache/cache.go
@@ -16,6 +16,13 @@ var (
 	mutex sync.RWMutex
 )
 
+const cleanPeriod = 10 * time.Minute
+
+func init() {
+	// 定时从cache map中清理已被删除的table
+	time.AfterFunc(cleanPeriod, cleanList)
+}
+
 type CheckFunc func(key interface{}) bool
 type UpdateCheckFunc func(value interface{}) bool
 
@@ -28,6 +35,17 @@ func GetTable(table string) (*Table, errs.InnerError) {
 	} else {
 		return item, nil
 	}
+}
+
+func cleanList() {
+	mutex.Lock()
+	defer mutex.Unlock()
+	for key, table := range cache {
+		if table.deleted {
+			delete(cache, key)
+		}
+	}
+	time.AfterFunc(cleanPeriod, cleanList)
 }
 
 func ListTable(filters ...TableFilter) ([]string, errs.InnerError) {
@@ -56,18 +74,24 @@ func Cache(table string, liveTime time.Duration) *Table {
 	mutex.RLock()
 	t, ok := cache[table]
 	mutex.RUnlock()
+	mutex.Lock()
 	if !ok {
-		mutex.Lock()
-		t, ok = cache[table]
-		if !ok {
-			t = &Table{
-				row:  make(map[interface{}]*Item),
-				name: table,
-			}
-			t.cleanupTimer = time.AfterFunc(liveTime, t.checkExpire)
-			cache[table] = t
-		}
-		mutex.Unlock()
+		t = newTable(table, liveTime)
+		cache[table] = t
+	} else if t.deleted {
+		delete(cache, table)
+		t = newTable(table, liveTime)
+		cache[table] = t
 	}
+	mutex.Unlock()
+	return t
+}
+
+func newTable(name string, liveTime time.Duration) *Table {
+	t := &Table{
+		row:  make(map[interface{}]*Item),
+		name: name,
+	}
+	t.cleanupTimer = time.AfterFunc(liveTime, t.checkExpire)
 	return t
 }
